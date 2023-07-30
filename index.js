d2cbf371bc364f4c62525398bbcfe0594df77100
habib1139 <hkhabib959@gmail.com>
2023-07-30 15:42:00 +0600

    even handler in comment box

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('HI There')
+document.getElementById('btn-post').addEventListener('click', function(){
+
+    const commentBox = document.getElementById('comment-area')
+    const comment = commentBox.value;
+
+   const commentText= document.getElementById('comment-text')
+   const p =document.createElement('p')
+   p.innerText = comment;
+   commentText.appendChild(p)
+
+   commentBox.value = ('');
+   
+})
